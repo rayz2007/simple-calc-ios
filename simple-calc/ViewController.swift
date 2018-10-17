b8e9d666f59ac3c47e62fed560208b6ef038f3d0
rayz2007 <rayz2007@uw.edu>
2018-10-16 21:51:48 -0700

    Initial Commit

---
+++ simple-calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-calc
+//
+//  Created by Ray Zhang on 10/16/18.
+//  Copyright © 2018 Ray Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
