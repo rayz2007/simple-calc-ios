37ae48a4e59a058fd499091fe99cbd7b23a5a262
rayz2007 <rayz2007@uw.edu>
2018-10-22 22:19:27 -0700

    added history

---
+++ simple-calc/HistoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryViewController.swift
+//  simple-calc
+//
+//  Created by Ray Zhang on 10/22/18.
+//  Copyright © 2018 Ray Zhang. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
